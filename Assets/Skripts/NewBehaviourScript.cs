e1db9e75f70e6b9a463be9a479b0c3638ce745d1
Tobias Behn <tobmoe33@gmail.com>
2022-12-12 16:49:07 +0100

    V2

---
+++ Assets/Skripts/NewBehaviourScript.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Oculus.Interaction;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+
+    public List<ActiveStateSelector> poses;
+    public TMPro.TextMeshPro text;
+    // Start is called before the first frame update
+    void Start()
+    {
+      foreach(var item in poses)
+        {
+            item.WhenSelected += () => SetTextToPoseName(item.gameObject.name);
+            item.WhenUnselected +=() => SetTextToPoseName("");
+        }
+    }
+
+    private void SetTextToPoseName(string newText)
+    {
+        text.text = newText;
+    }
+
+   
+}
