02f896e5b111df1492010b981d4d925f79fe6717
Tobias Behn <tobmoe33@gmail.com>
2023-02-02 15:03:00 +0100

    robosound Intro, robo animation intro implementiert

--- Assets/Skripts/RobotVoiceIntro.cs
+++ Assets/Skripts/RobotVoiceIntro.cs
@@ -8,36 +8,22 @@ using UnityEngine;
 public class RobotVoiceIntro : MonoBehaviour
 {
     public Renderer button;
-    public AudioSource robot;
-    public AudioClip[] earth;
-    public AudioClip[] jupiter;
-    public AudioClip[] mars;
-    public AudioClip[] mercury;
-    public AudioClip[] neptune;
-    public AudioClip[] saturn;
-    public AudioClip[] uranus;
-    public AudioClip[] venus;
-    public GameObject currentPlanet;
     private bool pressed = false;
-    public int index = 0;
-    public TextMeshPro text;
     public Animator anim;
     public AudioSource intro;
 
-    private void Start()
-    {
-        text.text = "null";
-    }
+   
 
     // Update is called once per frame
     void Update()
     {
-        if(button.material.color == Color.green && !pressed)
+        //anim.SetTrigger("rotate");
+    
+        if (button.material.color == Color.green && !pressed)
         {
 
             pressed = true;
-            anim.SetTrigger("rotate");
-            anim.SetTrigger("wave");
+            anim.Play("robotWave");
           
            
            
